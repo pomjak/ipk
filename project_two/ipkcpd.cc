8d0a5e26b02c8497a3452af778446daef5caf1de
xpomsa00 <pomjak71@gmail.com>
2023-04-12 19:49:57 +0200

    ipkcpd.cc: implementing udp communication, connection is stable

--- project_two/ipkcpd.cc
+++ project_two/ipkcpd.cc
@@ -1,3 +1,22 @@
+/*
+ * IPK Project 2: IOTA: Server for Remote Calculator
+ * Author: xpomsa00
+ * Copyright (C) 2023 Pomsar Jakub FIT VUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -21,7 +40,7 @@ using namespace std;
 #define REQUEST_OFFSET 2  // offset for reading from udp request
 #define RESPONSE_OFFSET 3 // offset for writing into udp response
 
-int client_socket; // global variables for signal handling
+int srv_socket; // global variables for signal handling
 int mode;
 bool close_soc = false;
 
@@ -38,7 +57,7 @@ void exit_err(string msg)
 {
     cerr << msg << endl;
     if (close_soc)
-        close(client_socket);
+        close(srv_socket);
     exit(EXIT_FAILURE);
 }
 
@@ -46,20 +65,20 @@ void signal_callback_handler(int signum)
 {
     if (mode == TCP)
     {
-        send(client_socket, "BYE\n", strlen("BYE\n"), 0); // if tcp, send BYE
+        send(srv_socket, "BYE\n", strlen("BYE\n"), 0); // if tcp, send BYE
         // SEND BYE
 
         char buffer[TCP_LIMIT];
-        recv(client_socket, buffer, TCP_LIMIT, 0); // and wait for BYE from server
+        recv(srv_socket, buffer, TCP_LIMIT, 0); // and wait for BYE from server
         cout << buffer;
     }
     if (close_soc)
-        close(client_socket);
+        close(srv_socket);
     // terminate program
     exit(signum);
 }
 
-void arg_processing(int argc, char *argv[], string &ip, int* port)
+void arg_processing(int argc, char *argv[], string &ip, unsigned int* port)
 {
     if (argc == 2 && string(argv[1]) == "--help")
         print_usage();
@@ -105,11 +124,43 @@ void arg_processing(int argc, char *argv[], string &ip, int* port)
 int main(int argc, char *argv[])
 {
     string ip;
-    int port_num = 0;
+    unsigned int port_num = 0;
 
     arg_processing(argc,argv,ip,&port_num);
 
+    if ((srv_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) // creating client socket for udp
+        exit_err("failed creating server socket");
+
+    struct sockaddr_in server_addr;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port_num);
+
+    if (bind(srv_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        exit_err("bind failed");
+
+    struct sockaddr_in client_addr;
+    socklen_t addr_size = sizeof(client_addr);
+    struct sockaddr *addr = (struct sockaddr *)&client_addr;
+
+    char buffer[UDP_LIMIT] = "\0";
+    int bytes_rx, bytes_tx;
+
     
+    bytes_rx = recvfrom(srv_socket, buffer, UDP_LIMIT, 0, addr, &addr_size);
+    if (bytes_rx < 0)
+        exit_err("ERROR: recvfrom");
+
     
+
+
+    // bytes_tx = sendto(srv_socket, buffer, strlen(buffer), 0, addr, addr_size);
+    // if (bytes_tx < 0)
+    //     exit_err("ERROR: sendto");
+    
+
     return EXIT_SUCCESS;
 }
