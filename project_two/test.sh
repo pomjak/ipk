2b2d73d3c17b877d303f56f7c9c9d91b09709731
xpomsa00 <pomjak71@gmail.com>
2023-04-17 00:35:56 +0200

    added tests

---
+++ project_two/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+    ../project_one/./ipkcpc -h 127.0.0.1 -p 2023 -m udp < tests/valid/"$i".in > tests/valid/"$i".out
+    echo "test "$i" :"
+    diff -s  tests/valid/"$i".out tests/valid/"$i".exp
+done
